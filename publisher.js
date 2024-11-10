c3a9f0b8ffe5379277c4befab2f1da679e054292
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-11-10 15:24:48 -0800

    edited publisher.js to pass current time by creating a new Date object and grabbing the hours, minutes and seconds

--- publisher.js
+++ publisher.js
@@ -1,18 +1,34 @@
-const zmq = require("zeromq")
+import zmq from "zeromq";
 
 async function run() {
-  const sock = new zmq.Publisher()
+  const sock = new zmq.Publisher();
 
-  await sock.bind("tcp://127.0.0.1:3000")
-  console.log("Publisher bound to port 3000")
+  await sock.bind("tcp://127.0.0.1:3000");
+  console.log("Publisher bound to port 3000");
 
   while (true) {
-    console.log("sending a multipart message envelope")
-    await sock.send(["kitty cats", "meow!"])
-    await new Promise(resolve => {
-      setTimeout(resolve, 500)
+    const current_time = new Date();
+    const hours = current_time.getHours();
+    const minutes = current_time.getMinutes();
+    const seconds = current_time.getSeconds();
+
+    
+    const messages = {
+      current_time: `${hours}:${minutes}:${seconds}`,
+      current_hours: hours.toString(),
+      current_minutes: minutes.toString(),
+      current_seconds: seconds.toString(),
+    }
+
+    for (const [topic, message] of Object.entries(messages)) {
+      console.log(`Sending ${topic}: ${message}`);
+      await sock.send([topic, message]);
+    }
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500);
     })
   }
 }
 
-run()
+run();
